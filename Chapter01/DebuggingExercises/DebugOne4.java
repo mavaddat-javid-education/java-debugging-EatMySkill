e48c91630fa9b99ad68e8a70c7b0e7e3e92f4985
BuildTools <unconfigured@null.spigotmc.org>
2020-06-05 12:20:08 -0700

    Fixed 3.3

--- Chapter01/DebuggingExercises/DebugOne4.java
+++ Chapter01/DebuggingExercises/DebugOne4.java
@@ -1,8 +1,7 @@
 import javax.swing.JOptionPane;
 public class DebugOne4
 {
-  public static main(String[] args)
-  {
-     JOptionPane.showMessageDialog(null, First GUI program)!
+  public static void main(String[] args) {  
+     JOptionPane.showMessageDialog(null, "Hello!");
   }
 }
