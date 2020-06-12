032e12a584f992408f172e628470d8e830bcca2f
EatMySkill <eatmyskilldude@gmail.com>
2020-06-12 12:36:13 -0700

    Finished DebugTwo1, DebugTwo2 and DebugTwo3
    Co-authored-by: Mavaddat Javid <javid@mavaddat.ca>

--- Chapter02/DebuggingExercises/DebugTwo3.java
+++ Chapter02/DebuggingExercises/DebugTwo3.java
@@ -3,12 +3,11 @@ public class DebugTwo3
 {
    public static void main(String args[])
    {
-      int a = 99, b = 8, result;
-      long c = 7777777777777;
-      result = a % b;
-      System.out.println("Divide " + a + " by " + b);
-      Systemout.println("remainder is " + a);
-      Systemout.print("c is a very large number: ");
-      Systemout.println(c);
+      int a = 99, b = 8;
+      long c = 7777777777777L;
+      System.out.println("Divide "+  a  +" by " + b + " is " + (a/b));
+      System.out.println("and the remainder is " +(a % b));
+      System.out.print("c is a very large number: ");
+      System.out.println(c);
     }
 }
