c4ea726b51ba525b74e162047d509ddd7dfbc967
EatMySkill <eatmyskilldude@gmail.com>
2020-07-03 12:15:24 -0700

    Finished 3.3

--- Chapter03/DebuggingExercises/DebugThree3.java
+++ Chapter03/DebuggingExercises/DebugThree3.java
@@ -1,22 +1,23 @@
 // This application gets a user's name and displays a greeting
 import java.util.Scanner;
+import java.util.jar.Attributes.Name;
 public class DebugThree3
 {
    public static void main(String args[])
    {
-      String name;
-      getName();
-      displayGreeting(name);           
-   }
+      String name = getName();
+      displayGreeting(name);
+   } 
    public static String getName()
    {
       String name;
       Scanner input = new Scanner(System.in);
-      System.out.print(Enter name );
-      name = input.nextInt();
+      System.out.println("Enter name" );
+      name = input.next();
+      input.close();
       return name;
    }
-   public static void displayGreeting()
+   public static void displayGreeting(String name)
    {
       System.out.println("Hello, " + name + "!");
    }
